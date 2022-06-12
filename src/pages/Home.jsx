85d438cbf9dfcbafe4db35febb26cd52e2e204c5
Alexis <yernauxalex@gmail.com>
2022-06-12 14:25:27 +0200

    Add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Login from '../components/Login';
+import Result from '../components/Result';
+import hash from '../utils/hash';
+var Spotify = require('spotify-web-api-js');
+
+var spotifyApi = new Spotify();
+
+
+function Home() {
+  const [token, setToken] = useState(null);
+
+  
+
+  useEffect(() => {
+    const accessToken = hash.access_token;
+    if (accessToken) {
+      setToken(accessToken);
+      spotifyApi.setAccessToken(accessToken);
+    }
+  }, [])
+  
+  return (
+    <div>
+      {token ? (
+        <Result token={token} />
+      ) : ( <Login /> )}
+    </div>
+  );
+}
+export default Home;
