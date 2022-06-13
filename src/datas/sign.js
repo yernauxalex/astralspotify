c1e50d316ccaefca0181519ff8e00289d893e06e
Alexis <yernauxalex@gmail.com>
2022-06-13 15:51:08 +0200

    Add datas

---
+++ src/datas/sign.js
@@ -0,0 +1,41 @@
+export const sign = [
+    {
+        name: 'aries',
+        unicode: '♈︎',
+    }, {
+        name: 'taurus',
+        unicode: '♉︎',
+    }, {
+        name: 'gemini',
+        unicode: '♊︎',
+    }, {
+        name: 'cancer',
+        unicode: '♋︎',
+    }, {
+        name: 'leo',
+        unicode: '♌︎',
+    }, {
+        name: 'virgo',
+        unicode: '♍︎',
+    }, {
+        name: 'libra',
+        unicode: '♎︎',
+    }, {
+        name: 'scorpio',
+        unicode: '♏︎',
+    }, {
+        name: 'sagittarius',
+        unicode: '♐︎',
+    }, {
+        name: 'capricorn',
+        unicode: '♑︎',
+    }, {
+        name: 'aquarius',
+        unicode: '♒︎',
+    }, {
+        name: 'pisces',
+        unicode: '♓︎',
+    }
+];
+
+
