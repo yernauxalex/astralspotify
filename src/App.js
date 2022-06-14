ffe168140d02eefba4b6795f8acd583e4ea800a1
Alexis <yernauxalex@gmail.com>
2022-06-14 18:10:04 +0200

    Fix redirection from spotify

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
