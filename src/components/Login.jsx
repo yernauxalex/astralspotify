69ac723d782c016d0e46c168ef73d375b0f2780a
Alexis <yernauxalex@gmail.com>
2022-06-17 15:40:57 +0200

    Fix env var for netlify

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { authEndpoint, clientId, redirectUri, scopes } from '../env';
+//import { authEndpoint, clientId, redirectUri, scopes } from '../env';
 import StyledLogin from '../styles/StyledLogin';
 
 
 
 function Login() {
+  const authEndpoint = process.env.authEndpoint
+  const clientId = process.env.clientId
+  const redirectUri = process.env.redirectUri
+  const scopes = process.env.scopes
   return (
     <StyledLogin>
       <a href={`${authEndpoint}?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes}`}>Login with Spotify</a>
