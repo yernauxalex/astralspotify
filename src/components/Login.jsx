14fce92c219772b7a4974903a378969187fbe8b7
Alexis <yernauxalex@gmail.com>
2022-06-19 12:05:26 +0200

    Fix redirecturi

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,14 +4,12 @@ import StyledLogin from '../styles/StyledLogin';
 
 
 function Login() {
-  const authEndpoint = 'https://accounts.spotify.com/authorize'
-  const clientId = '43bfdb7c53c642d8bfd73c2d94c64983'
-  const redirectUri = 'https://62ac84f931585f0009dd42b8--resonant-medovik-c1c915.netlify.app/'
-  const scopes = ['user-top-read',]
+  // const authEndpoint = 'https://accounts.spotify.com/authorize'
+  // const scopes = ['user-top-read',]
 
   return (
     <StyledLogin>
-      <a href={`${authEndpoint}?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes}`}>Login with Spotify</a>
+      <a href={`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENDID}&response_type=token&redirect_uri=${process.env.REACT_APP_REDIRECTURI}&scope=user-top-read`}>Login with Spotify</a>
     </StyledLogin>
   );
 }
