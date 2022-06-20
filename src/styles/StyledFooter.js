c8ab6bd3af19366aba5eb2b980b5889ff8ac5035
Alexis <yernauxalex@gmail.com>
2022-06-20 08:21:39 +0200

    Add media queries + fonts

--- src/styles/StyledFooter.js
+++ src/styles/StyledFooter.js
@@ -1,18 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import fonts from './fonts';
 
 const StyledFooter = styled.footer`
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    height: 5rem;   
-    width: 100%;
-
-    img {
-        min-width: 70px;
-        min-height: 70px;
-    }
-`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 5rem;
+  width: 100%;
+  font-size: ${fonts.standard.footer};
+  img {
+    min-width: 70px;
+    min-height: 70px;
+  }
+  @media (max-width: 600px) {
+    font-size: ${fonts.media600.footer};
+  }
+  @media (max-width: 280px) {
+    font-size: ${fonts.media280.footer};
+  }
+`;
 
 export default StyledFooter;
--- src/styles/StyledResult.js
+++ src/styles/StyledResult.js
@@ -1,40 +1,45 @@
 import styled from 'styled-components';
 import colors from './colors';
+import fonts from './fonts';
 
 const StyledResult = styled.section`
-display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+  width: 100%;
+  font-size: ${fonts.standard.mainSection};
+  .container-select {
+    display: flex;
+    flex-direction: raw;
     align-items: center;
     justify-content: center;
-    height: 80%;
-    width: 100%;
-    font-size: 1.5rem; 
-    .container-select {
-        display: flex;
-        flex-direction: raw;
-        align-items: center;
-        justify-content: center;
-        gap: 1rem;
-        padding: 1rem;
+    gap: 1rem;
+    padding: 1rem;
+  }
+  input {
+    border: 1px solid ${colors.text};
+    padding: 0.5rem;
+    margin: 0.5rem;
+    border: 1px solid ${colors.text};
+    background-color: ${colors.spotifyGreen};
+    padding: 1rem;
+    border-radius: 5px;
+    &:hover {
+      color: ${colors.secondary};
     }
-    input{
-       border: 1px solid ${colors.text};
-       padding: 0.5rem;
-       margin: 0.5rem;
-       border: 1px solid ${colors.text};
-       background-color: ${colors.spotifyGreen};
-       padding: 1rem;
-       border-radius: 5px;
-       &:hover {
-        color: ${colors.secondary};
-       }
-    }
-    a {
-        text-decoration: none;
-        color: black;
-        font-size: 1rem;
-
-    }
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+  @media (max-width: 600px) {
+    font-size: ${fonts.media600.mainSection};
+  }
+  @media (max-width: 280px) {
+    font-size: ${fonts.media280.mainSection};
+  }
 `;
 
 export default StyledResult;
