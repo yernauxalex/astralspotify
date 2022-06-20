8641923e62d991b305c60709acd8df53c03ea0e2
Alexis <yernauxalex@gmail.com>
2022-06-20 12:03:15 +0200

    Add link to footer

--- src/styles/StyledFooter.js
+++ src/styles/StyledFooter.js
@@ -4,15 +4,21 @@ import fonts from './fonts';
 const StyledFooter = styled.footer`
   position: relative;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   height: 5rem;
   width: 100%;
   font-size: ${fonts.standard.footer};
+  p {
+    margin: 0;
+  }
   img {
     min-width: 70px;
     min-height: 70px;
+  }
+  a {
+    text-decoration: none;
   }
   @media (max-width: 600px) {
     font-size: ${fonts.media600.footer};
