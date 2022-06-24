e6303dea1a2d905ad08613bbe93f5f540557293d
Alexis <yernauxalex@gmail.com>
2022-06-24 19:10:20 +0200

    WIP UserVote + css

--- src/components/UserVote.jsx
+++ src/components/UserVote.jsx
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import StyledVote from "../styles/StyledVote";
 // import { genre } from "../datas/genre";
 import { sign } from "../datas/sign";
 
 function UserVote(props) {
     const userGenre = props;
-    console.log(userGenre);
-    console.log("props", props);
+    const [userVote, setUserVote] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userVote);
+    }
 
     return (
         <StyledVote>
             <h2>Help us to get a more accurate matching</h2>
             <form>
-
+                <div className="genre-container" key="label">
+                    <label key="label"> Sign </label>
+                    <div className="sign-container-list" >
+                        {sign.map((sign) => (
+                            <div className="sign-container-desc" key={"label" + sign.name}>
+                                <input
+                                    type="radio"
+                                    name={sign.name}
+                                    value={sign.name}
+                                    className="sign-radio"
+                                />
+                                <label>{sign.unicode}</label>
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 {Object.values(userGenre).map((genre) => (
-                    <div className="genre" key={genre}>
+                    <div className="genre-container" key={genre}>
                         <label key={genre}> {genre} </label>
-                        <select value={genre} >
+                        <div className="sign-container-list" >
+                            {sign.map((sign) => (
+                                <div className="sign-container" key={sign.name}>
+                                    <input
+                                        type="radio"
+                                        name={genre}
+                                        value={sign.name}
+                                        onChange={(e) =>
+                                            setUserVote({ ...userVote, [genre]: e.target.value })} />
+                                </div>
+                            ))}
+                        </div>
+                        {/* <select value={userVote} key={genre + "select"} onChange={(e) => setUserVote(e.target.value)}>
                             {sign.map(({ name, unicode }) => (
                                 <option key={name} value={name}>
                                     {name} {unicode}
                                 </option>
                             ))}
-                        </select>
+                        </select> */}
                     </div>
                 ))}
-
             </form>
-        </StyledVote>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+        </StyledVote >
     );
 }
 export default UserVote
--- src/styles/StyledVote.js
+++ src/styles/StyledVote.js
@@ -2,6 +2,42 @@ import styled from 'styled-components';
 // import colors from './colors';
 // import fonts from './fonts';
 
-const StyledVote = styled.section``;
+const StyledVote = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .genre-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        padding-bottom: 0.2rem;
+    }
+    #label {
+        font-size: 1.2rem;
+        font-weight: bold;
+        color: #fff;
+        padding-bottom: 0.2rem;
+    }
+    .sign-container-desc {
+        min-width: 1.5rem;
+    }
+    .sign-container-desc > input {
+        appearance: none;
+    }
+    .genre-container > label {
+        padding-right: 0.5rem;
+    }
+    .sign-container-list {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+    .sign-container {
+        min-width: 1.5rem;
+    }
+`;
 
 export default StyledVote;
