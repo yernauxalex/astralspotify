761a3c7c7dbb14075f2f64d90f17c01df7cd32b6
Alexis <yernauxalex@gmail.com>
2022-06-24 19:10:05 +0200

    Fix missing key

--- src/components/DisplayGenre.jsx
+++ src/components/DisplayGenre.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
 function DisplayGenre(props) {
-const { data }  = props;
-console.log(data);
+    const { data } = props;
+    console.log(data);
     return (
         <ul className="main-list">
-            {data.map(({ name, external_urls ,genres }) => (
-                // eslint-disable-next-line react/jsx-key
-                <a href={external_urls.spotify} target="_blank" rel="noreferrer">
-                <li key={name} className="main-list-items">
-                    <h3>{name}</h3>
-                    <ul className="secondary-list">
-                        {genres.map(( name ) => (
-                            <li key={name} className="secondary-list-items">{name}</li>
-                        ))}
+            {data.map(({ name, external_urls, genres }) => (
+                <a href={external_urls.spotify} target="_blank" rel="noreferrer" key={external_urls.spotify}>
+                    <li key={name} className="main-list-items">
+                        <h3>{name}</h3>
+                        <ul className="secondary-list">
+                            {genres.map((name) => (
+                                <li key={name} className="secondary-list-items">{name}</li>
+                            ))}
                         </ul>
-                </li>
+                    </li>
                 </a>
             ))}
         </ul>
