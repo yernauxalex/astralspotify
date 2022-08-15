7130192bf32fa50b8b60187097ada602864f39e9
Alexis <yernauxalex@gmail.com>
2022-08-15 15:33:41 +0200

    WIP Bootstrap header + login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import fonts from '../styles/fonts';
 
 function Header() {
     return (
-        <header>
-            <h1>Astro compatibility for Spotify</h1>
-        </header>
+        <Navbar className="d-flex justify-content-center">
+            <Navbar.Brand href="/" style={{ fontSize: fonts.standard.h1 }}>
+                Astro compatibility for Spotify
+            </Navbar.Brand>
+        </Navbar>
     )
 }
 export default Header
