fdd72915d1cac8d6c8105e1b75ae25c9578e9ca9
Alexis <yernauxalex@gmail.com>
2022-10-28 11:55:38 +0200

    Remove footer

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-      <Footer />
-    </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
