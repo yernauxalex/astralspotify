f01c01d75ae28f69cc1a39676ad3212efb763617
Alexis <yernauxalex@gmail.com>
2022-11-10 18:04:43 +0100

    WIP layout extendedVote

--- src/pages/ExtendedVote.jsx
+++ src/pages/ExtendedVote.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Button, Container } from "react-bootstrap";
 import { sign } from '../datas/sign';
 import { genre } from '../datas/genre';
 
@@ -24,22 +24,27 @@ function ExtendedVote() {
                 setFormData(newFormData);
             }
         } else {
-            console.log('index', signIndex);
             newFormData.push({ sign: selectedSign, vote: [{ genre: selectedGenre, value: 0 }] });
             setFormData(newFormData);
         }
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
-        <>
-            <div>
-                <h1>Extended Vote</h1>
-            </div>
-            <Form>
-                <Form.Group as={Row} className="mb-3 d-flex justify-content-center" controlId="formGroupSign">
-                    <Form.Label column md={2} xs="auto">Enter your sign</Form.Label>
-                    <Col md={3} xs="auto">
-                        <Form.Select value={selectedSign} onChange={(e) => setSelectedSign(e.target.value)} aria-label="enter your astro sign">
+        <Container fluid>
+            <Container className="my-3 d-flex flex-column  align-items-center">
+                <h2>Extended Vote</h2>
+                <p>Here you can rate the affinity between each music genre and astro sign, to gve us a better accurary in our result</p>
+            </Container>
+            <Form as={Container} className="d-flex flex-column justify-content-center">
+                <Form.Group className="mb-3 d-flex flex-column flex-sm-row justify-content-center" controlId="formGroupSign">
+                    <Form.Label column md={2} xs="auto" className="me-3">Select a sign</Form.Label>
+                    <Col md={4} sm={5} xs="auto">
+                        <Form.Select value={selectedSign} onChange={(e) => setSelectedSign(e.target.value)} aria-label="select an astro sign">
                             {sign.map(({ name, unicode }) => (
                                 <option key={name} value={name}>
                                     {name} {unicode}
@@ -48,10 +53,10 @@ function ExtendedVote() {
                         </Form.Select>
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row} className="mb-3 d-flex justify-content-center" controlId="formGroupSign">
-                    <Form.Label column md={2} xs="auto">Enter your sign</Form.Label>
-                    <Col md={3} xs="auto">
-                        <Form.Select value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)} aria-label="enter your astro sign">
+                <Form.Group className="mb-3 d-flex flex-column flex-sm-row justify-content-center" controlId="formGroupSign">
+                    <Form.Label column md={2} xs="auto" className="me-3">Select a genre</Form.Label>
+                    <Col md={4} sm={5} xs="auto">
+                        <Form.Select value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)} aria-label="select a music genre">
                             {genre.map(({ name }, index) => (
                                 <option key={index} value={name}>
                                     {name}
@@ -61,37 +66,50 @@ function ExtendedVote() {
                     </Col>
                 </Form.Group>
                 <Form.Group className="d-flex justify-content-center" controlId="formGroupButton">
-                    <Button variant="success" type="submit" className="w-50" onClick={addInput}>Found your matching sign</Button>
+                    <Button variant="success" type="submit" className="w-50" onClick={addInput}>Create your selection</Button>
                 </Form.Group>
             </Form>
-            <Form as={Row} className="mb-3 d-flex justify-content-center" controlId="formGroupSign">
+            <Form className="mb-3 d-flex flex-column align-items-center" controlId="formGroupSign">
                 {formData.map((data, indexData) => (
-                    <>
-                        <Form.Label column md={2} xs="auto">{data.sign}</Form.Label>
+                    <Container key={indexData} className="mt-3 d-flex flex-column custom-selection">
+                        <Form.Label >{data.sign}</Form.Label>
                         {data.vote.map((vote, index) => (
-                            <Form.Group as={Row} className="mb-3 d-flex justify-content-center" controlId="formGroupSign" key={index}>
-                                <Form.Label column sm="3">
+                            <Form.Group className="mb-3 d-flex flex-column flex-md-row justify-content-center align-items-center" controlId="formGroupSign" key={index}>
+                                <Form.Label column md={3} xs="auto">
                                     {vote.genre}
                                 </Form.Label>
-                                <Col md={3} xs="auto">
-                                    <Form.Range
-                                        value={vote.value}
-                                        onChange={(e) => {
-                                            const newFormData = [...formData];
-                                            newFormData[indexData].vote[index].value = e.target.value;
-                                            setFormData(newFormData);
-                                        }}
-                                    />
-                                </Col>
-                                <Col md={1} xs="auto">
-                                    <Form.Control value={vote.value} />
-                                </Col>
+                                <Row className="d-flex justify-content-center">
+                                    <Col >
+                                        <Form.Range
+                                            min={0}
+                                            max={100}
+                                            value={vote.value}
+                                            onChange={(e) => {
+                                                const newFormData = [...formData];
+                                                newFormData[indexData].vote[index].value = e.target.value;
+                                                setFormData(newFormData);
+                                            }}
+                                            aria-valuemin={0}
+                                            aria-valuemax={100}
+                                            aria-valueNow={vote.value}
+                                        />
+                                    </Col>
+                                    <Col md={3} xs={3}>
+                                        <Form.Control value={vote.value} readOnly />
+                                    </Col>
+                                </Row>
                             </Form.Group>
                         ))}
-                    </>
+                    </Container>
                 ))}
             </Form>
-        </>
+            {formData.length > 0 && (
+                <Form.Group className="d-flex justify-content-center" controlId="formGroupButton">
+                    <Button variant="success" type="submit" onClick={handleSubmit}>Submit</Button>
+                </Form.Group>
+            )}
+
+        </Container>
     );
 }
 export default ExtendedVote;
